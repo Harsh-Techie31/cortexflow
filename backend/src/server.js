520d0c912ebc9c84e9a89860306365ac5a1e2bbe
Harsh <harshchoudhary3113@gmail.com>
2026-02-15 06:45:24 +0530

    connected mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
+
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
 
+const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 
-dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.use(cors());
 app.use(express.json());
